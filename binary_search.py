b753579fae8581e1087fb0b8fa0023219b87d66d
Federico Perez <federicoperezramirez@gmail.com>
2024-09-25 10:59:42 -0500

    binary search algorithm
    it doesnt have an ordered list

---
+++ binary_search.py
@@ -0,0 +1,35 @@
+import random
+
+def random_list():
+    lis = []
+    for i in range(100):
+        lis.append(random.randint(0,40))
+        
+    return lis
+
+def bianary_search(numbers, target):
+    first = numbers[0]
+    last = len(numbers) - 1
+    while first <= last:
+        mid = (first + last) //2
+
+        if numbers[mid] == target:
+            return mid
+        elif numbers[mid] < target:
+            first = mid + 1
+        else:
+            last = mid - 1 
+
+    return None
+
+def verify(index):
+    if index is not None:
+        print(f'target found at index {index}')
+    else:
+        print('target not found in list')
+
+numbers = random_list() # not ordered list
+#TODO Add an algorithm that orders an array
+result = bianary_search(numbers,32)
+verify(result)
+print(numbers)

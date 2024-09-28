f9110a9cc36dc9ed76028b67b07d7df48031504a
Nicolas Duque Gutierrez <n.duque1@utp.edu.co>
2024-09-28 03:11:57 -0500

    No more memory QS

--- quicksort.py
+++ quicksort.py
@@ -1,6 +1,5 @@
 from binary_search import random_list
 import numpy as np
-import time
 
 
 def quicksort(vector):
@@ -14,7 +13,7 @@ def quicksort(vector):
     if start < end:
         right_pos = locate_pivot(vector, start, end)
 
-        quicksort(vector[:right_pos-1])
+        quicksort(vector[:right_pos])
         quicksort(vector[right_pos+1:])
 
 
@@ -22,25 +21,24 @@ def quicksort(vector):
 
 
 def locate_pivot(vector, start, end):
+    print(f"Este para esta iteracion: {vector}")
+    print(f"pivote {vector[start]}")
     while start < end:
         while vector[end] >= vector[start] and start < end:
             end -= 1
-            print(f"pivote derecha {start} {end}")
-            #time.sleep(0.1)
 
         vector[start], vector[end] = vector[end], vector[start]
 
         while vector[start] <= vector[end] and start < end:
             start += 1
-            print(f"pivote izquierda: {start} {end}")
-            #time.sleep(0.1)
+
         vector[start], vector[end] = vector[end], vector[start]
     return start
 
 
 
-numbers = random_list() # not ordered list
-numbers = np.array(numbers)
-print(f"Nor ordered list: {numbers}")
-quicksort(numbers)
-print(f"Ordered list: {numbers}")
+test_vec = random_list() # not ordered list
+test_vec = np.array(test_vec)
+print(f"Nor ordered list: {test_vec}")
+quicksort(test_vec)
+print(f"Ordered list: {test_vec}")
